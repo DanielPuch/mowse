bf9e34ec36af431d80bd647ecdee178aeb2db74c
Daniel Puch <danielpuch1969@gmail.com>
2016-09-01 15:39:18 -0400

    First Commit
    Theme for mowse design website

---
+++ template-parts/homepage.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Homepage 
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<div id="slides">
+    <div class="slides-container">
+     <img src="../images/people.jpeg" alt="Cinelli">
+      <img src="../images/surly.jpeg" width="1024" height="682" alt="Surly">
+      <img src="../images/cinelli-front.jpeg" width="1024" height="683" alt="Cinelli">
+      <img src="../images/affinity.jpeg" width="1024" height="685" alt="Affinity">
+    </div>
+
+    <nav class="slides-navigation">
+      <a href="#" class="next">Next</a>
+      <a href="#" class="prev">Previous</a>
+    </nav>
+  </div>
+
+
+<?php get_footer(); ?>
